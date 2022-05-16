76c5ffb6bdf732746be6e4231fdaf7b48a670f2f
r-tamura <rui-tamura@omniment.co.jp>
2022-05-17 04:39:01 +0900

    [fix] マウントが不足していたのを修正

--- recore-lighttpd/install.sh
+++ recore-lighttpd/install.sh
@@ -33,7 +33,7 @@ docker stop $APP_NAME"_"
 docker load -i $INSTALL_FILES/$IMG_NAME
 
 RUN_STATE=0
-docker run -d --privileged --net=host -v /usr/bin/systemctl:/usr/bin/systemctl -v /etc/hostname:/etc/hostname -v /usr/local/bin/recore/files:/usr/local/bin/recore/files -v /usr/local/bin/recore/install:/usr/local/bin/recore/install -v /etc/hostapd/hostapd.conf:/etc/hostapd/hostapd.conf -v /etc/wpa_supplicant/wpa_supplicant.conf:/etc/wpa_supplicant/wpa_supplicant.conf -v /run/systemd/system:/run/systemd/system -v /var/run/dbus/system_bus_socket:/var/run/dbus/system_bus_socket -v /sys/fs/cgroup:/sys/fs/cgroup -v /usr/local/bin/recore/files/version:/usr/local/bin/recore/files/version -v /home/recore/fusion-files/:/home/recore/fusion-files/ --name recore-lighttpd recore-lighttpd:$VERSION lighttpd -f /etc/lighttpd/lighttpd.conf -D && RUN_STATE=1
+docker run -d --privileged --net=host -v /usr/bin/systemctl:/usr/bin/systemctl -v /etc/hostname:/etc/hostname -v /etc/hosts:/etc/hosts -v /usr/local/bin/recore/files:/usr/local/bin/recore/files -v /usr/local/bin/recore/install:/usr/local/bin/recore/install -v /etc/hostapd/hostapd.conf:/etc/hostapd/hostapd.conf -v /etc/wpa_supplicant/wpa_supplicant.conf:/etc/wpa_supplicant/wpa_supplicant.conf -v /run/systemd/system:/run/systemd/system -v /var/run/dbus/system_bus_socket:/var/run/dbus/system_bus_socket -v /sys/fs/cgroup:/sys/fs/cgroup -v /usr/local/bin/recore/files/version:/usr/local/bin/recore/files/version -v /home/recore/fusion-files/:/home/recore/fusion-files/ --name recore-lighttpd recore-lighttpd:$VERSION lighttpd -f /etc/lighttpd/lighttpd.conf -D && RUN_STATE=1
 
 if [ $RUN_STATE = 1 ];then
 	docker rm -f $APP_NAME"_"
