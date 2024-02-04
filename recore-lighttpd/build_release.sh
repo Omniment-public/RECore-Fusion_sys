760a0a9341615cbcc8d9bf631d94e7e7a075e247
r-tamura <rui-tamura@omniment.co.jp>
2024-02-04 21:19:54 +0900

    [update] ツールにgz圧縮処理を追加

--- recore-lighttpd/build_release.sh
+++ recore-lighttpd/build_release.sh
@@ -5,3 +5,6 @@
 # build
 docker build -t recore-lighttpd:$1 .
 docker save recore-lighttpd:$1 -o "recore-lighttpd-image.tar"
+
+# compress
+tar -zcvf recore-lighttpd-image.tar.gz recore-lighttpd-image.tar
