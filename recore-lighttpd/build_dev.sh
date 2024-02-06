301108297b74dc160fb2fa78676247cd66781126
r-tamura <rui-tamura@omniment.co.jp>
2024-02-07 01:17:06 +0900

    [update] リリースパッケージ作成ツールを追加

--- recore-lighttpd/build_dev.sh
+++ recore-lighttpd/build_dev.sh
@@ -2,6 +2,14 @@
 
 #argv $1:version
 
+# check args
+if [ $# != 1 ]; then
+    echo "need version args"
+    exit 1
+fi
+
+cd "$(dirname "$0")"
+
 # build
 docker build -t recore-lighttpd-test:$1 .
 
--- recore-lighttpd/build_release.sh
+++ recore-lighttpd/build_release.sh
@@ -2,9 +2,20 @@
 
 #argv $1:version
 
+# check args
+if [ $# != 1 ]; then
+    echo "need version args"
+    exit 1
+fi
+
+cd "$(dirname "$0")"
+
 # build
 docker build -t recore-lighttpd:$1 .
+
+echo "export image"
 docker save recore-lighttpd:$1 -o "recore-lighttpd-image.tar"
 
 # compress
+echo "compress"
 tar -zcvf recore-lighttpd-image.tar.gz recore-lighttpd-image.tar
