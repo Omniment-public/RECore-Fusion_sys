15047505207c1803244a9b6035b29efb713c8729
r-tamura <rui-tamura@omniment.co.jp>
2025-06-23 02:00:24 +0900

    [update] 起動時の挙動を修正

--- raspberry-pi/boot_recore.sh
+++ raspberry-pi/boot_recore.sh
@@ -9,7 +9,7 @@ pinctrl set $LED_GREEN op dl  # LCD off
 pinctrl set $LED_RED   op dl
 
 WLAN=wlan0
-CON_AP="recore-ap"            # 事前に nmcli で作成
+CON_AP="ap-recore"            # 事前に nmcli で作成
 STA_PREFIX="sta-"             # STA プロファイル命名規則
 NMCLI=/usr/bin/nmcli
 
@@ -25,17 +25,23 @@ sudo bash /usr/local/bin/recore/files/update.sh
 WLAN_MODE=$(</usr/local/bin/recore/files/wlan_mode)    # 0=STA /1=AP
 #logger -t boot_recore "start wlan_mode=$WLAN_MODE"
 
+sudo nmcli device set $WLAN managed yes
+
 ### 1. STA モード
 
 if [[ "$WLAN_MODE" == "0" ]]; then
     echo "=== STA Mode ==="
-	nmcli device set $WLAN managed yes
-	if ! nmcli connection up id "${STA_PREFIX}"\*; then
-		WLAN_STATE=""
-	else
-		sleep 6
-		WLAN_STATE=$(iwgetid -r || true)
-	fi
+	
+    sudo nmcli device connect $WLAN
+    
+    sleep 10
+
+    # 接続試行
+    if nmcli -t -f DEVICE,STATE,CONNECTION device status | grep -q '^wlan0:connected:'; then
+        WLAN_STATE="CONNECT"   # 成功
+    else
+        WLAN_STATE=""          # 失敗
+    fi
 else
     WLAN_STATE=""
 fi
@@ -44,13 +50,18 @@ fi
 if [[ -z "$WLAN_STATE" ]]; then
     echo "=== AP Mode ==="
 
-	# 自動チャネル選択 (失敗時は ch6)
-    best_ch=$(python3 /usr/local/bin/recore/files/wlan_autochannel_nm.py) || best_ch=1
+    sleep 5
+	
+    # 自動チャネル選択 (失敗時は ch6)
+    best_ch=$(python3 /usr/local/bin/recore/files/wlan_autochannel.py) || best_ch=1
     if [[ $best_ch -ge 1 && $best_ch -le 13 ]]; then
-        nmcli connection modify $CON_AP wifi.channel "$best_ch"
+        sudo nmcli connection modify $CON_AP wifi.channel "$best_ch"
         echo "[boot_recore] AP channel set to $best_ch"
     fi
 
+    # STA無効化
+    sudo nmcli device disconnect $WLAN
+    
 	# AP プロファイルを up （NM が hostapd + 内蔵 dnsmasq を自動起動）
     sudo nmcli connection up $CON_AP
 
--- raspberry-pi/wlan_autochannel.py
+++ raspberry-pi/wlan_autochannel.py
@@ -11,7 +11,7 @@ for i in stdstr:
     if(int(i) < 13):
         channel_list[int(i) - 1] += 1
 
-print(channel_list)
+# print(channel_list)
 
 use_ch = channel_list.index(min(channel_list)) + 1
 print(use_ch)
